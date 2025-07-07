e845c6cfbe923db3f3d593f0daaae0ffd5f71acd
Vickycroce <vickymesoni124@gmail.com>
2025-07-07 00:12:21 -0300

    funcion de buscador

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,52 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { CarrouselComponent } from "./carrousel/carrousel.component";
 import { AuthService } from '../../services/auth.service';
 import { Router} from '@angular/router';
+import { ProductosService, Producto } from '../productos/productos.service';
 
 @Component({
   selector: 'app-home',
-  imports: [CarrouselComponent],
+  standalone: true,
+  imports: [CarrouselComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  productos: Producto[] = [];
+  productosFiltrados: Producto[] = [];
 
- constructor(private authService: AuthService, private router: Router) {}
-  irAPerfumes() {
+ constructor(private authService: AuthService, private router: Router,  private productosService: ProductosService,) {}
+
+ masVendidos: Producto[] = [];
+
+ngOnInit(): void {
+  this.productosService.obtenerProductos().subscribe({
+    next: (data) => {
+      this.productos = data;
+      this.productosFiltrados = data;
+
+      this.masVendidos = this.productos.slice(0, 5);
+    },
+    error: (err) => console.error(err)
+  });
+}
+filtrarProductos(event: Event): void {
+  const inputElement = event.target as HTMLInputElement;
+  const valor = inputElement?.value?.trim().toLowerCase() || '';
+
+  if (!valor) {
+    this.productosFiltrados = this.productos;
+    return;
+  }
+
+  this.productosFiltrados = this.productos.filter(p =>
+    p.nombre.toLowerCase().includes(valor)
+  );
+}
+
+
+ irAPerfumes() {
     this.router.navigate(['/productos']);
   }
 
