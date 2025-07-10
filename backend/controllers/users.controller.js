4ae61e6930275d7a1c9ffe980f3d85aff274e7c5
Vickycroce <vickymesoni124@gmail.com>
2025-07-09 22:15:00 -0300

    Ver perfil terminado

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -22,4 +22,22 @@ const loginUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser, loginUser };
+const getPerfil = async (req, res) => {
+  try {
+    const usuario = await userService.getById(req.usuario.id);
+    res.json(usuario);
+  } catch (err) {
+    res.status(404).json({ mensaje: err.message });
+  }
+};
+
+const getById = async (req, res) => {
+  try {
+    const usuario = await userService.getById(req.params.id);
+    if (!usuario) return res.status(404).json({ message: 'Usuario no encontrado' });
+    res.json(usuario);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+module.exports = { registerUser, loginUser, getPerfil, getById };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { canActivateAuth } from './guards/auth.guard';
 import { VistaProductoComponent } from './pages/vista-producto/vista-producto.component';
 import { MisPedidosComponent } from './pages/mis-pedidos/mis-pedidos.component';
+import { PerfilComponent } from './perfil/perfil.component';
+
 
 
 
@@ -16,6 +18,7 @@ export const routes: Routes = [
   { path: 'productos', component: ProductosComponent, canActivate: [canActivateAuth] },
   { path: 'home', component: HomeComponent, canActivate: [canActivateAuth] },
   { path: 'vista-producto/:id', component: VistaProductoComponent, canActivate: [canActivateAuth] },
-  {path: 'mis-pedidos', component: MisPedidosComponent, canActivate: [canActivateAuth]}
+  {path: 'mis-pedidos', component: MisPedidosComponent, canActivate: [canActivateAuth]},
+  { path: 'perfil', component: PerfilComponent },
 
 ];
